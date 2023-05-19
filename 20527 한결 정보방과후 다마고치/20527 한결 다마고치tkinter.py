2ff0cbedb024e4efddd279e95720c32296443016
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-05-19 18:07:00 +0900

    Add files via upload

--- 20527 한결 정보방과후 다마고치/20527 한결 다마고치tkinter.py
+++ 20527 한결 정보방과후 다마고치/20527 한결 다마고치tkinter.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random as rd
 #변수
+petList=[]
 fnt=("맑은 고딕 Semilight",15)
 pet_img=[
                     tk.PhotoImage(file="run.png"),
@@ -32,42 +33,53 @@ class ceobe:
     petName=""
     petAge=0
     petMeal=0
-    petWater
+    petNameEntry=0
+    petNameWindow=0
+    fnt = ("맑은 고딕 Semilight", 15)
 
     #실행시키자마자 사용
-    def __init__(self,new_petName,new_petAge):
-        self.petName=new_petName
-        self.petAge=new_petAge
+    def __init__(self):
 
-    def __del__(self):
-        print(self.petName, "를 떠나보냅니다... ㅠㅠ\n")
+        self.petNameWindow = tk.Tk()
+        self.petNameWindow.title("이름 짓기")
+        self.petNameWindow.geometry("450x230")
+        self.petNameWindow.resizable(False, False)
 
-    def petInfoPrint(self):
-        print("이름:",self.petName)
-        print("나이:",self.petAge)
-        print("[상태]:")
+        petNameLabel=tk.Label(self.petNameWindow,text="케오의 별명을 지어주세요.",font=fnt,padx=20,pady=20)
+        petNameLabel.pack()
 
-        if self.petMeal==0:
-            print("배고픈 상태...\n")
-        else:
-            print("행복한 상태! \n")
-    
-    def petMealAdd(self):
+        self.petNameEntry=tk.Entry(self.petNameWindow,font=fnt)
+        self.petNameEntry.pack()
+
+        btnOK=tk.Button(self.petNameWindow,text="확인",font=fnt,command=self.petNameSet)
+        btnOK.pack()
+
+    def petNameSet(self):
+        self.petName=self.petNameEntry.get()
+        self.petNameWindow.destroy()
+        
+        petNameLabel=tk.Label(root,text="이름:"+self.petName,fg="green",font=fnt)
+        petNameLabel.place(x=170,y=430)
+        self.petImageSelect()
+        
+    def petImageSelect(self):
+        
+
+
+    def peteatMeal(self):
         self.petMeal+=1
         canvas.create_text(100,100,text="meal+1",fill="pink",tag="text")
 
         root.after(1000,self.petMealDel)
 
-    def petMealDel(self):
-        canvas.delete("text")
+    def peteatWater(self):
+        self.petWater += 1
+        x = rd.randint(20, 380)
+        y = rd.randint(20, 380)
+        canvas.create_text(x, y, text="water+1", fill="blue", font=fnt, tag="text")
+        root.after(500, self.peteatDel())
 
-    def petWaterAdd(self):
-        self.water+=1
-        canvas.create_text(100,100,text="water+1",fill="blue",tag="text")
-
-        root.after(1000,self.petWaterDel)
-
-    def petWaterDel(self):
+    def peteatDel(self):
         canvas.delete("text")
                            
 
@@ -75,6 +87,9 @@ def main():
     canvas.create_image(200,200,file=pet_img[0],tag="pet")
 
 
+def petAdd():
+    temp=ceobe()
+    petList.append(temp)
 
 #메인
 root=tk.Tk()
@@ -82,43 +97,45 @@ root.title("조조 키우기")
 root.geometry("440x550")
 root.resizable(False,False)
 
+petAdd()
+
 canvas=tk.Canvas(width=400,height=400,bg="white")
 canvas.place(x=20,y=20)
 
-add_btn=tk.Button(text="추가",bg="white",fg="green",font=fnt)
-add_btn.place(x=20,y=430)
+add_btn=tk.Button(text="추가",bg="white",fg="green",font=fnt,command=petAdd)
+add_btn.place(x=20,y=480)
 
-import tk as tk
-#변수
-fnt=("맑은 고딕 Semilight",15)
+waterImg=tk.PhotoImage(file="water.png")
+waterImg=waterImg.subsample(6)
+waterBtn=tk.Button(image=waterImg,command=petList)
+waterBtn.place(x=110,y=480)
 
+meatImg=tk.PhotoImage(file="meat.png")
+meatImg=waterImg.subsample(6)
+meatBtn=tk.Button(image=meatImg)
+meatBtn.place(x=180,y=480)
 
-#함수
+meatBtn=tk.Button(image=meatImg)
+meatBtn.place(x=180,y=430)
 
-#메인
-root=tk.Tk()
-root.title("조조 키우기")
-root.geometry("440x550")
-root.resizable(False,False)
+pet_img=[
+                    tk.PhotoImage(file="hmm.png"),
+                    tk.PhotoImage(file="hello.png"),
+                    tk.PhotoImage(file="ike.png")
+                    tk.PhotoImage(file="makaseryo.png"),
+                    tk.PhotoImage(file="hello2(gif).gif")
+    ]
 
-first=ceobe()
-canvas=tk.Canvas(width=400,height=400,bg="white")
-canvas.place(x=20,y=20)
-
-add_btn=tk.Button(text="추가",bg="white",fg="green",font=fnt,command=first.petWaterAdd)
-add_btn.place(x=20,y=430)
-
-water_img=tk.PhotoImage(file="water.png")
-water_img=water_img.subsample(6)
-
-water_btn=tk.Button(image=water_img)
-water_btn.place(x=100,y=430)
-
-meat_img=tk.PhotoImage(file="meat.png")
-meat_img=water_img.subsample(6)
-
-meat_btn=tk.Button(image=meat_img)
-meat_btn.place(x=180,y=430)
+petPicSelect1=tk.PhotoImage(file="hmm.png")
+petPicSelect1.subsample((6))
+petPicSelect2=tk.PhotoImage(file="hello.png")
+petPicSelect2.subsample((6))
+petPicSelect3=tk.PhotoImage(file="ike.png")
+petPicSelect3.subsample((6))
+petPicSelect4=tk.PhotoImage(file="makaseryo.png")
+petPicSelect4.subsample((6))
+petPicSelect5=tk.PhotoImage(file="hello2(gif).gif")
+petPicSelect5.subsample((6))
 
 main()
 root.mainloop()
