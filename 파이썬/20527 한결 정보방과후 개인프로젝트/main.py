1f3d3ce4a91619973b49b4b19c25a566f68e381e
KR_kkunis <domino1492@kakao.com>
2023-06-11 00:38:28 +0900

    프로젝트업데이트

---
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import random as rd
+
+gameMenuList=["공격","행동","창고","도망"]
+
+
+class charMe():         #자기자신
+    def charMeKeypress(e):
+        key=e.keysym
+        if key==
+
+
+class charEnemy1():
+class charEnemy2():
+
+
+
+
+def main():
+    pass
+
+root=tk.Tk()
+root.title("쯔꾸르게임")
+root.geometry("720x540")
+root.resizable(False,False)
+root.bind("<KeyPress>", charMe.charMeKeypress)
+canvas=tk.Canvas(root,width=720,height=540,bg="white")
+canvas.pack()
+
+charMeImg=[tk.PhotoImage(file="공격1.gif"),
+                tk.PhotoImage(file="공격2.png"),
+                tk.PhotoImage(file="공격3.png"),
+                tk.PhotoImage(file="기본.png"),
+                tk.PhotoImage(file="섭취.png"),
+                tk.PhotoImage(file="피격.png")
+                ]
+
+main()
+root.mainloop()
