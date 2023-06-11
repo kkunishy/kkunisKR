b7de27203172714ce2a75cd9118aae625f2b6d3e
KR_kkunis <domino1492@kakao.com>
2023-06-11 22:45:55 +0900

    Update main.py

--- 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
@@ -2,17 +2,15 @@ import tkinter as tk
 import random as rd
 
 gameMenuList=["공격","행동","창고","도망"]
-
+fnt = ("맑은 고딕 Semilight", 20)
 
 class charMe():         #자기자신
     def charMeKeypress(e):
         key=e.keysym
         if key==
 
-
 class charEnemy1():
 class charEnemy2():
-
 
 
 
@@ -24,7 +22,7 @@ root.title("쯔꾸르게임")
 root.geometry("720x540")
 root.resizable(False,False)
 root.bind("<KeyPress>", charMe.charMeKeypress)
-canvas=tk.Canvas(root,width=720,height=540,bg="white")
+canvas=tk.Canvas(root,width=720,height=540,bg="black")
 canvas.pack()
 
 charMeImg=[tk.PhotoImage(file="공격1.gif"),
