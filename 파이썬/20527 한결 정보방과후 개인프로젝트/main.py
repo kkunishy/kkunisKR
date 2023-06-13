8aeb53fe48aa757c3d838c7cbfca8793e6d33525
KR_kkunis <domino1492@kakao.com>
2023-06-13 17:38:40 +0900

    Update main.py

--- 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random as rd
+gameMenuNum=0
 
 gameMenuList=["공격","행동","창고","도망"]
 fnt = ("맑은 고딕 Semilight", 20)
@@ -7,7 +8,24 @@ fnt = ("맑은 고딕 Semilight", 20)
 class charMe():         #자기자신
     def charMeKeypress(e):
         key=e.keysym
-        if key==
+        if key=="Down" and gameMenuNum=1:
+            gameMenuNum=3
+        elif key=="Down" and gameMenuNum=2:
+            gameMenuNum=4
+        elif key=="Up" and gameMenuNum=3:
+            gameMenuNum=1
+        elif key=="Up" and gameMenuNum=4:
+            gameMenuNum=2
+        elif key=="Left" and gameMenuNum=2:
+            gameMenuNum=1
+        elif key=="Left" and gameMenuNum=4
+            gameMenuNum=3
+        elif key=="Right" and gameMenuNum=1:
+            gameMenuNum=2
+        elif key=="Right" and gameMenuNum=3:
+            gameMenuNum=4
+        else:
+            pass
 
 class charEnemy1():
 class charEnemy2():
