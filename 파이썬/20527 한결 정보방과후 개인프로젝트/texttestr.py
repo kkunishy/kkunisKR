95835007278a7070958bf127550789ecdc614ea8
KR_kkunis <domino1492@kakao.com>
2023-06-13 17:32:56 +0900

    Update texttestr.py

--- 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
@@ -12,8 +12,10 @@ root.resizable(False,False)
 canvas=tk.Canvas(root,width=720,height=540,bg="black")
 canvas.pack()
 
-gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt)
-gameMenuLabel1.place(x=180,y=480)
+canvas.create_rectangle(0,350,720,360,fill="white")
+
+gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg="white",bg="black")
+gameMenuLabel1.place(x=400,y=400)
 gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt)
 
 gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt)
