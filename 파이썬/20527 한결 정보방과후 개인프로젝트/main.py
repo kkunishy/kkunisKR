6492937dc11d277d56fff4c2c60b43cbf6bc2c69
KR_kkunis <domino1492@kakao.com>
2023-06-13 18:08:08 +0900

    asdf

--- 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/main.py
@@ -1,47 +1,115 @@
 import tkinter as tk
 import random as rd
+
+gameMenuList=["*공격","*행동","*창고","*도망"]
 gameMenuNum=0
+fnt = ("맑은 고딕 Semilight", 15)
+fnt2 = ("맑은 고딕 Semilight", 20)
+colorList=["white","black"]     #[0] 하양 [1] 검정
+labelFG1=""
+labelFG2=""
+labelFG3=""
+labelFG4=""
+labelBG1=""
+labelBG2=""
+labelBG3=""
+labelBG4=""
 
-gameMenuList=["공격","행동","창고","도망"]
-fnt = ("맑은 고딕 Semilight", 20)
 
-class charMe():         #자기자신
-    def charMeKeypress(e):
-        key=e.keysym
-        if key=="Down" and gameMenuNum=1:
-            gameMenuNum=3
-        elif key=="Down" and gameMenuNum=2:
-            gameMenuNum=4
-        elif key=="Up" and gameMenuNum=3:
-            gameMenuNum=1
-        elif key=="Up" and gameMenuNum=4:
-            gameMenuNum=2
-        elif key=="Left" and gameMenuNum=2:
-            gameMenuNum=1
-        elif key=="Left" and gameMenuNum=4
-            gameMenuNum=3
-        elif key=="Right" and gameMenuNum=1:
-            gameMenuNum=2
-        elif key=="Right" and gameMenuNum=3:
-            gameMenuNum=4
-        else:
-            pass
+def gameMenuKeypress(e):
+    key=e.keysym
+    if key=="Down" and gameMenuNum==1:
+        gameMenuNum=3
+    elif key=="Down" and gameMenuNum==2:
+        gameMenuNum=4
+    elif key=="Up" and gameMenuNum==3:
+        gameMenuNum=1
+    elif key=="Up" and gameMenuNum==4:
+        gameMenuNum=2
+    elif key=="Left" and gameMenuNum==2:
+        gameMenuNum=1
+    elif key=="Left" and gameMenuNum==4:
+        gameMenuNum=3
+    elif key=="Right" and gameMenuNum==1:
+        gameMenuNum=2
+    elif key=="Right" and gameMenuNum==3:
+        gameMenuNum=4
+    else:
+        pass
 
-class charEnemy1():
-class charEnemy2():
+        
+#class charEnemy1():
+#class charEnemy2():
 
 
 
 def main():
     pass
 
+def gameMenuselect1():
+    global gameMenuNum, labelFG1, labelFG2, labelFG3, labelFG4, labelBG1, labelBG2, labelBG3, labelBG4, colorList
+    gameMenuNum=1
+    labelFG1=colorList[1]
+    labelFG2=colorList[0]
+    labelFG3=colorList[0]
+    labelFG4=colorList[0]
+    labelBG1=colorList[1]
+    labelBG2=colorList[0]
+    labelBG3=colorList[0]
+    labelBG4=colorList[0]
+def gameMenuselect2():
+    gameMenuNum=2
+    labelFG1=colorList[0]
+    labelFG2=colorList[1]
+    labelFG3=colorList[0]
+    labelFG4=colorList[0]
+    labelBG1=colorList[0]
+    labelBG2=colorList[1]
+    labelBG3=colorList[0]
+    labelBG4=colorList[0]
+def gameMenuselect3():
+    gameMenuNum=3
+    labelFG1=colorList[0]
+    labelFG2=colorList[0]
+    labelFG3=colorList[1]
+    labelFG4=colorList[0]
+    labelBG1=colorList[0]
+    labelBG2=colorList[0]
+    labelBG3=colorList[1]
+    labelBG4=colorList[0]
+def gameMenuselect4():
+    gameMenuNum=4
+    labelFG1=colorList[0]
+    labelFG2=colorList[0]
+    labelFG3=colorList[0]
+    labelFG4=colorList[1]
+    labelBG1=colorList[0]
+    labelBG2=colorList[0]
+    labelBG3=colorList[0]
+    labelBG4=colorList[1]
+    
 root=tk.Tk()
 root.title("쯔꾸르게임")
 root.geometry("720x540")
 root.resizable(False,False)
-root.bind("<KeyPress>", charMe.charMeKeypress)
+root.bind("<KeyPress>",gameMenuKeypress)
 canvas=tk.Canvas(root,width=720,height=540,bg="black")
 canvas.pack()
+
+canvas.create_rectangle(0,350,720,360,fill="white")
+
+gameQuestionLabel=tk.Label(text="무엇을 하시겠습니까?",font=fnt2,bg="black",fg="white")
+gameQuestionLabel.place(x=50,y=420)
+
+gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg=labelFG1,bg=labelBG1)
+gameMenuLabel1.place(x=400,y=400)
+gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt,fg=labelFG2,bg=labelBG2)
+gameMenuLabel2.place(x=550,y=400)
+gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt,fg=labelFG3,bg=labelBG3)
+gameMenuLabel3.place(x=400,y=460)
+gameMenuLabel4=tk.Label(text=gameMenuList[3],font=fnt,fg=labelFG4,bg=labelBG4)
+gameMenuLabel4.place(x=550,y=460)
+
 
 charMeImg=[tk.PhotoImage(file="공격1.gif"),
                 tk.PhotoImage(file="공격2.png"),
--- 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 
-gameMenuList=["공격","행동","창고","도망"]
+gameMenuList=["*공격","*행동","*창고","*도망"]
 gameMenuNum=0
 fnt = ("맑은 고딕 Semilight", 15)
+fnt2 = ("맑은 고딕 Semilight", 20)
+labelFG="white"     #폰트
+labelBG="black"     #배경
 
 root=tk.Tk()
 root.title("쯔꾸르게임")
@@ -14,10 +17,14 @@ canvas.pack()
 
 canvas.create_rectangle(0,350,720,360,fill="white")
 
-gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg="white",bg="black")
+gameQuestionLabel=tk.Label(text="무엇을 하시겠습니까?",font=fnt2,bg="black",fg=labelFG)
+gameQuestionLabel.place(x=50,y=420)
+
+gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg=labelFG,bg=labelBG)
 gameMenuLabel1.place(x=400,y=400)
-gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt)
-
-gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt)
-
-gameMenuLabel4=tk.Label(text=gameMenuList[3],font=fnt)
+gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel2.place(x=550,y=400)
+gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel3.place(x=400,y=460)
+gameMenuLabel4=tk.Label(text=gameMenuList[3],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel4.place(x=550,y=460)
