3cf207f8e8a2a5683f4c1109fdf50fff0dc315a9
KR_kkunis <domino1492@kakao.com>
2023-06-15 17:07:02 +0900

    Update texttestr.py

--- 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
+++ 파이썬/20527 한결 정보방과후 개인프로젝트/texttestr.py
@@ -4,8 +4,8 @@ gameMenuList=["*공격","*행동","*창고","*도망"]
 gameMenuNum=0
 fnt = ("맑은 고딕 Semilight", 15)
 fnt2 = ("맑은 고딕 Semilight", 20)
-labelFG="white"     #폰트
-labelBG="black"     #배경
+colorList=["white","black"]     #[0] 하양 [1] 검정
+
 
 root=tk.Tk()
 root.title("쯔꾸르게임")
@@ -20,11 +20,11 @@ canvas.create_rectangle(0,350,720,360,fill="white")
 gameQuestionLabel=tk.Label(text="무엇을 하시겠습니까?",font=fnt2,bg="black",fg=labelFG)
 gameQuestionLabel.place(x=50,y=420)
 
-gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel1=tk.Label(text=gameMenuList[0],font=fnt,fg=labelFG1,bg=labelBG1)
 gameMenuLabel1.place(x=400,y=400)
-gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel2=tk.Label(text=gameMenuList[1],font=fnt,fg=labelFG2,bg=labelBG2)
 gameMenuLabel2.place(x=550,y=400)
-gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel3=tk.Label(text=gameMenuList[2],font=fnt,fg=labelFG3,bg=labelBG3)
 gameMenuLabel3.place(x=400,y=460)
-gameMenuLabel4=tk.Label(text=gameMenuList[3],font=fnt,fg=labelFG,bg=labelBG)
+gameMenuLabel4=tk.Label(text=gameMenuList[3],font=fnt,fg=labelFG4,bg=labelBG4)
 gameMenuLabel4.place(x=550,y=460)
