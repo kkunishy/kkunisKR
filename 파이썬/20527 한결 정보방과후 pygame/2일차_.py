f30bd127ccd2e48831664204b8660cb38e32cc91
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-08-29 16:34:35 +0900

    d

--- 파이썬/20527 한결 정보방과후 pygame/2일차_.py
+++ 파이썬/20527 한결 정보방과후 pygame/2일차_.py
@@ -1,14 +1,16 @@
 import pygame as pg
 import sys
+#전역변수
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+RED=(255,0,0)
 
 #시작
 pg.init()
-
-#창띄우기
+#디스플레이
 pg.display.set_caption("와 샌즈")
 screen=pg.display.set_mode((760,920))
 screen.fill((0,0,255))
-
 running=True
 while running:
     for event in pg.event.get():
@@ -16,6 +18,4 @@ while running:
             pg.quit()
             sys.exit()
     pg.display.update()
-
-#끝
 pg.quit()
