d6a5e951252c62753b0fbaa84870120bdc6dc3e3
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-08-30 16:33:27 +0900

    Update 3일차_.py

--- 파이썬/20527 한결 23학년 2학기 정보방과후/3일차_.py
+++ 파이썬/20527 한결 23학년 2학기 정보방과후/3일차_.py
@@ -6,7 +6,7 @@ import pygame.display
 #시작
 pg.init()
 pg.display.set_caption("그림 띄우기")
-screen=pg.display.set_mode(960,720)
+screen=pg.display.set_mode((960,720))
 #진행
 running=True
 
@@ -17,10 +17,10 @@ while running:
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_F1:
                 screen=pg.display.set_mode((960,720))
-                screen.fill(200,100,180)
+                screen.fill((200,100,180))
             if event.key==pg.K_F2 or event.key==pg.K_SPACE:
                 screen=pg.display.set_mode((960,720),pg.FULLSCREEN)
-                screen.fill(180,200,100)
+                screen.fill((180,200,100))
         pygame.display.update()
 pg.quit()
 sys.exit()
