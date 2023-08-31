00a9072add85c3706b5662bdeb77db6cfaa98ed3
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-08-31 16:33:14 +0900

    Update 3일차_.py

--- 파이썬/20527 한결 23학년 2학기 정보방과후/3일차_.py
+++ 파이썬/20527 한결 23학년 2학기 정보방과후/3일차_.py
@@ -9,7 +9,7 @@ pg.display.set_caption("그림 띄우기")
 screen=pg.display.set_mode((960,720))
 #이미지 링크
 img_ceobe=pg.image.load("케오베.png")
-font1=pygame.font.SysFont("맑은 고딕 Semilight",30)
+font1=pygame.font.SysFont("맑은 고딕 Semilight",100)
 #진행
 running=True
 
@@ -26,7 +26,13 @@ while running:
                 screen.fill((180,200,100))
             if event.key==pg.K_SPACE:
                 screen.fill((0,0,0))
+        key=pg.key.get_pressed()
+        #^^^  key[pygame.K_UP]=1 을 출력
         screen.blit(img_ceobe,[350,200])
+        txt1=font1.render("Up key enabled:"+str(key[pg.K_UP]),True,(0,255,255),(0,0,0))
+        screen.blit(txt1,[100,100])
+        txt2=font1.render("your dad",True,(0,255,255),(0,0,0))
+        screen.blit(txt2,[100,300])
         pygame.display.update()
 pg.quit()
 sys.exit()
