7a9b9a02c9d02380ea2a96dedcf054c60475a936
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-09-04 16:39:46 +0900

    Update 6일차.py

---
+++ 파이썬/20527 한결 23학년 2학기 정보방과후/6일차.py
@@ -0,0 +1,31 @@
+import pygame as pg
+import sys
+
+#전역변수 영역
+#이미지 영역
+img_bg=pg.image.load()
+#함수 영역
+#시작영역
+pg.init()
+pg.display.set_caption()
+screen=pg.display.set_mode((960,720))
+clock=pg.time.Clock()
+
+
+#진행 영역
+running=True
+
+while running:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            running=False
+        if event.type==pg.KEYDOWN:
+            if event.key==pg.K_F1:
+                screen=pg.display.set_mode((960,720))
+            if event.key==pg.K_F2:
+                screen=pg.display.set_mode((960,720),pg.FULLSCREEN)
+    clock.tick(50)
+    pg.display.update()
+
+pg.quit()
+sys.exit()
