45db1c120a10d5eed78f61b73eab4c25ce59b18b
KR_kkunis <128043057+domino1492@users.noreply.github.com>
2023-09-07 17:03:45 +0900

    Add files via upload

--- 파이썬/20527 한결 23학년 2학기 정보방과후/6일차.py
+++ 파이썬/20527 한결 23학년 2학기 정보방과후/6일차.py
@@ -2,14 +2,25 @@ import pygame as pg
 import sys
 
 #전역변수 영역
+class bullet():
+    x=0
+    y=0
+    a=0
+    speed=0
+    def __init__(self,x,y,a,speed):
+        self.x=x
+        self.y=y
+        self.a=a
+        self.speed=speed
+
 bg_scroll=0
 ss_x=(960/2)-37
 ss_y=(720/2)+48
 ss_speed=3
 ss_img_num=0
-bullet_x=-100
-bullet_y=-100
-bullet_speed=0
+b_list=[]
+b_t=0
+b_ts=100
 #이미지 영역
 img_bg=pg.image.load("galaxy.png")
 img_ss=[
@@ -22,7 +33,7 @@ img_bul=pg.image.load("bullet.png")
 
 #함수 영역
 def move_ship(screen,key):
-    global ss_y,ss_x,ss_speed,ss_img_num,bullet_x,bullet_y,bullet_speed
+    global b_t,ss_y,ss_x,ss_speed,ss_img_num,b_ts
 
     if key[pg.K_LEFT]==1 and ss_x>0:
         ss_x-=ss_speed
@@ -40,15 +51,23 @@ def move_ship(screen,key):
         ss_speed+=5
     if key[pg.K_2]==1:
         ss_speed-=5
+        
     if key[pg.K_SPACE]==1:
-        bullet_x=ss_x+27
-        bullet_y=ss_y
-        bullet_speed=30
+        if b_t>=b_ts:
+            b_t=0
+            b=bullet(ss_x+27,ss_y,0,10)
+            b_list.append(b)
+def draw_bullet(screen):
+    for i in b_list:
+        screen.blit(img_bul,[i.x,i.y])
+        i.y-=i.speed
 
+        if i.y<0:
+            b_list.remove(i)
 #시작영역
 #진행 영역
 def main():
-    global bg_scroll,ss_x,ss_y,ss_speed,ss_img_num,bullet_x,bullet_y,bullet_speed
+    global b_t,bg_scroll,ss_x,ss_y,ss_speed,ss_img_num,b_ts
     pg.init()
     pg.display.set_caption("동방프로젝트 해적판wwwww")
     screen = pg.display.set_mode((960, 720))
@@ -56,6 +75,7 @@ def main():
     running=True
 
     while running:
+        b_t+=1
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 running=False
@@ -65,6 +85,11 @@ def main():
                     screen=pg.display.set_mode((960,720))
                 if event.key==pg.K_F2:
                     screen=pg.display.set_mode((960,720),pg.FULLSCREEN)
+                if event.key==pg.K_9:
+                    b_ts+=5
+                if event.key==pg.K_0:
+                    b_ts-=5
+                
         key=pg.key.get_pressed()
         move_ship(screen,key)
         screen.blit(img_bg,[0,bg_scroll-720])
@@ -74,14 +99,16 @@ def main():
         if bg_scroll%10==0:
             screen.blit(img_ss[3],[ss_x+29, ss_y+100])
         screen.blit(img_ss[ss_img_num],[ss_x,ss_y])
-
-        screen.blit(img_bul,[bullet_x,bullet_y])
-        bullet_y-=bullet_speed
+        draw_bullet(screen)
         clock.tick(50)
         if ss_speed <= 20:
             ss_speed = 21
         if ss_speed>50:
             ss_speed=49
+        if b_ts<1:
+            b_ts=1
+        print(b_ts)
+        print(b_t)
         pg.display.update()
     pg.quit()
     sys.exit()
