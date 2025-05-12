32c7cd8a023d258c617e308ec8f98c7f752d2fc7
kkunisKR <domino1492@kakao.com>
2025-05-12 10:30:30 +0900

    repo

--- 파이썬/2504110119 한결 25학년 1학기/4for.py
+++ 파이썬/2504110119 한결 25학년 1학기/4for.py
@@ -7,7 +7,11 @@ for i in range(employee_num):
     if rank>=2:
         bonus=basic_salary*0.3
     else:
-        bonus=basic_salary*0.2
-    salary=basic_salary+bonus
-    total+=salary
-print('월급총액: ',total)
+        bonus = basic_salary * 0.2
+    salary = basic_salary + bonus
+    total += salary
+    print(f'본봉: {basic_salary}원')
+    print(f'보너스: {bonus}원')
+    print(f'총 급여: {salary}원')
+    
+print('\n전체 월급총액:', total, '원')
