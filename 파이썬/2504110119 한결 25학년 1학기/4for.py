ac587600dd23903983704f33eb0457c3b0ec529a
kkunisKR <domino1492@kakao.com>
2025-05-12 10:30:58 +0900

    rre

--- 파이썬/2504110119 한결 25학년 1학기/4for.py
+++ 파이썬/2504110119 한결 25학년 1학기/4for.py
@@ -1,11 +1,10 @@
-employee_num=3
-total=0
-
+employee_num = 3
+total = 0
 for i in range(employee_num):
-    rank=int(input('직급: '))
-    basic_salary=int(input('본봉: '))
-    if rank>=2:
-        bonus=basic_salary*0.3
+    rank = int(input('직급: '))
+    basic_salary = int(input('본봉: '))
+    if rank >= 2:
+        bonus = basic_salary * 0.3
     else:
         bonus = basic_salary * 0.2
     salary = basic_salary + bonus
