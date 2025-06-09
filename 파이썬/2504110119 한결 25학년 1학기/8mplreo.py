7b15ba3a3fa7b46e2efed92c1ee5b519e100ad5e
kkunisKR <domino1492@kakao.com>
2025-06-09 11:46:47 +0900

    Update 8mplreo.py

--- 파이썬/2504110119 한결 25학년 1학기/8mplreo.py
+++ 파이썬/2504110119 한결 25학년 1학기/8mplreo.py
@@ -1,16 +1,2 @@
 import matplotlib.pyplot as plt
-
-name =['Jessica', 'Liam', 'Sophia']
-
-x = [75, 92, 83]
-y = [79, 89, 90]
-plt.figure(figsize=(5,3))
-plt.scatter(x, y)
-plt.annotate(name[0],(x[0], y[0]), xytext=(5,-10),textcoords='offset points')
-plt.annotate(name[1],(x[1], y[1]), xytext=(5,-10),textcoords='offset points')
-plt.annotate(name[2],(x[2], y[2]), xytext=(5,-10),textcoords='offset points')
-plt.xlim(73, 95)
-plt.ylim(75, 93)
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
+import os
