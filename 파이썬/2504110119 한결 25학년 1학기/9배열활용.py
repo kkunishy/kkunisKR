be8d53d8d9730f6423695a5ba7db949ed49f0f29
kkunisKR <domino1492@kakao.com>
2025-06-23 09:03:48 +0900

    Update 9배열활용.py

--- 파이썬/2504110119 한결 25학년 1학기/9배열활용.py
+++ 파이썬/2504110119 한결 25학년 1학기/9배열활용.py
@@ -6,9 +6,9 @@ women=[163.2, 163.3, 158.5,164.5]
 d=np.arange(len(nation))
 plt.figure(figsize=(5,3))
 plt.bar(ind-0.2, men, color='b',width=0.4, label='men')
-plt.bar(ind+0.2, women, color='r'. width=0.4, label='women')
+plt.bar(ind+0.2, women, color='r', width=0.4, label='women')
 plt.ylim(130, 190)
-plt.title('Height'
+plt.title('Height')
 plt.ylabel('cm')
 plt.xticks@nnd, nation)
 plt.legend()
